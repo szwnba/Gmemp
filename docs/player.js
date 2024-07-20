ee04aa136896f0b9133554161bc5e4ce45cb5c52
Johnson zhang <szwnba@qq.com>
2024-07-20 12:38:21 +0800

    Update player.js

--- docs/player.js
+++ docs/player.js
@@ -1,4 +1,4 @@
-let media="https://cdn.jsdelivr.net/gh/Meekdai/Gmemp@main/media/"
+let media="media/"
 
 // Cache references to DOM elements.
 let elms = ['track','artist', 'timer', 'duration','post', 'playBtn', 'pauseBtn', 'prevBtn', 'nextBtn', 'playlistBtn', 'postBtn', 'waveBtn', 'volumeBtn', 'progress', 'progressBar','waveCanvas', 'loading', 'playlist', 'list', 'volume', 'barEmpty', 'barFull', 'sliderBtn'];
